5f16efe00fbe42e76cd8d199ead9fdd313d628c6
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-18 18:00:46 -0800

    Update DbSets and add Identity

--- PierresTreats/Models/PierresTreatsContext.cs
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace PierresTreats.Models
 {
-  public class PierresTreatsContext : DbContext
+  public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
   {
     public DbSet<Flavor> Flavors { get; set; }
     public DbSet<Treat> Treats { get; set; }
-    public DbSet<FlavorTreat> FlavorTreat { get; set;}
+    public DbSet<FlavorTreat> FlavorTreats { get; set;}
     public PierresTreatsContext(DbContextOptions options) : base(options) { }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
