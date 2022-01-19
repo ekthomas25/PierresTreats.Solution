1634586c673340eccccb241d8d679f14ead01734
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-18 17:21:38 -0800

    Update naming and add DbSet

---
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PierresTreats.Models
+{
+  public class PierresTreatsContext : DbContext
+  {
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set;}
+    public PierresTreatsContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
