05ca8bdcb04400e9037768fc5eb0f3e67acf5f37
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-18 18:10:46 -0800

    Add routing for splash page

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,9 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Dynamic;
+using PierresTreats.Models;
 
 namespace PierresTreats.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresTreatsContext _db;
+    public HomeController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
 
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      dynamic model = new ExpandoObject();
+      model.Flavors = _db.Flavors.ToList();
+      model.Treats = _db.Treats.ToList();
+      return View(model);
+    }
   }
 }
