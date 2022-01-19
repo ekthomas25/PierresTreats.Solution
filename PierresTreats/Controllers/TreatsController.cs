01a3708065cda6e8663f74b53fb3cdbb9ceed5ac
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-18 17:25:13 -0800

    Fix syntax error in name

---
+++ PierresTreats/Controllers/TreatsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PierresTreats.Models;
+
+namespace PierresTreats.Controllers
+{
+  public class TreatsController : Controller
+  {
+
+  }
+}
