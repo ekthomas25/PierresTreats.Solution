89ec22a26219862225c2b41a0cfcba214a26a0a5
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-19 21:19:41 -0800

    Update for authorization using identity

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -2,37 +2,50 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 using PierresTreats.Models;
 
 namespace PierresTreats.Controllers
 {
+  [Authorize]
   public class TreatsController : Controller
   {
     private readonly PierresTreatsContext _db;
-    public TreatsController(PierresTreatsContext db)
+    private readonly UserManager<ApplicationUser> _userManager;
+    public TreatsController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
-
+    [AllowAnonymous]
     public ActionResult Index()
     {
       return View(_db.Treats.ToList());
     }
 
+    
     public ActionResult Create()
     {
       return View();
     }
 
+    
     [HttpPost]
-    public ActionResult Create(Treat treat)
+    public async Task<ActionResult> Create(Treat treat)
     {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
       _db.Treats.Add(treat);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
-        public ActionResult Details(int id)
+    [AllowAnonymous]
+    public ActionResult Details(int id)
     {
       var thisTreat = _db.Treats
           .Include(treat => treat.JoinEntities)
@@ -41,12 +54,13 @@ namespace PierresTreats.Controllers
       return View(thisTreat);
     }
 
+    
     public ActionResult Edit(int id)
     {
       Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
       return View(thisTreat);
     }
-
+    
     [HttpPost]
     public ActionResult Edit(Treat treat)
     {
@@ -55,12 +69,14 @@ namespace PierresTreats.Controllers
       return RedirectToAction("Details", new { id = treat.TreatId });
     }
 
-        public ActionResult Delete(int id)
+    
+    public ActionResult Delete(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
       return View(thisTreat);
     }
 
+    
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
@@ -70,13 +86,15 @@ namespace PierresTreats.Controllers
       return RedirectToAction("Index");
     }
 
-        public ActionResult AddFlavor(int id)
+    
+    public ActionResult AddFlavor(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
       ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
       return View(thisTreat);
     }
 
+    
     [HttpPost]
     public ActionResult AddFlavor(Treat treat, int FlavorId)
     {
@@ -88,6 +106,7 @@ namespace PierresTreats.Controllers
       return RedirectToAction("Details", new { id = treat.TreatId });
     }
 
+    
     [HttpPost]
     public ActionResult DeleteFlavor(int joinId, int treatId)
     {
