37cda23bb09c10b090ed77747ed78d5aa462ae5b
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-19 21:33:05 -0800

    Add authorization for authenticated users

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -2,18 +2,26 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 using PierresTreats.Models;
 
 namespace PierresTreats.Controllers
 {
+  [Authorize]
   public class FlavorsController : Controller
   {
     private readonly PierresTreatsContext _db;
-    public FlavorsController(PierresTreatsContext db)
+    private readonly UserManager<ApplicationUser> _userManager;
+    public FlavorsController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
 
+    [AllowAnonymous]
     public ActionResult Index()
     {
       return View(_db.Flavors.ToList());
@@ -26,13 +34,17 @@ namespace PierresTreats.Controllers
     }
 
     [HttpPost]
-    public ActionResult Create(Flavor flavor)
+    public async Task<ActionResult> Create(Flavor flavor)
     {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      flavor.User = currentUser;
       _db.Flavors.Add(flavor);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
+    [AllowAnonymous]
     public ActionResult Details(int id)
     {
       var thisFlavor = _db.Flavors
--- PierresTreats/Models/Flavor.cs
+++ PierresTreats/Models/Flavor.cs
@@ -12,6 +12,7 @@ namespace PierresTreats.Models
     public int FlavorId { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
+    public virtual ApplicationUser User { get; set; }
     public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
   }
 }
